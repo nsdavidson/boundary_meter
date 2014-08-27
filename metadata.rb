56b8781d687efc0e0a0c5f5b2912ef714ec8e919
Nolan Davidson <nolan.davidson@gmail.com>
2014-08-26 21:19:20 -0400

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'boundary_meter'
+maintainer       'Nolan Davidson'
+maintainer_email 'nolan.davidson@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures boundary_meter'
+long_description 'Installs/Configures boundary_meter'
+version          '0.1.0'
+
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: boundary_meter
+# Recipe:: default
+#
+# Copyright (C) 2014 Nolan Davidson
+#
+# All rights reserved - Do Not Redistribute
+#
