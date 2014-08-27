bf6f869f904479edd0ffb46c3f8cd81a1965c15e
Nolan Davidson <nolan.davidson@gmail.com>
2014-08-27 11:21:24 -0400

    Base yum install working

--- recipes/install_linux.rb
+++ recipes/install_linux.rb
@@ -1,6 +1,23 @@
-package 'boundary-meter'
+etc_path = node[:boundary][:meter][:etc_path]
 
-template "#{node[:boundary][:meter][:etc_path]}/meter.conf" do
+directory "#{etc_path}"
+template "#{etc_path}/meter.conf" do
   source 'meter.conf.erb'
   mode 600
+end
+
+package 'boundary-meter'
+
+install_token = "#{node[:boundary][:api][:org_id]}:#{node[:boundary][:api][:key]}"
+
+bash 'provision' do
+  cwd node[:boundary][:meter][:bin_path]
+  code <<-EOH
+    ./boundary-meter-provision -i #{install_token}
+    EOH
+  not_if { ::File.exists?("#{etc_path}/key.pem") }
+end
+
+service 'boundary-meter' do
+  action [:enable, :start]
 end
