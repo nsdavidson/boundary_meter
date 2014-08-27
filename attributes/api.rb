37c9938d01066dd6a28506db67463e464b8b9f10
Nolan Davidson <nolan.davidson@gmail.com>
2014-08-26 21:35:34 -0400

    Initial population of default attributes

---
+++ attributes/api.rb
@@ -0,0 +1,4 @@
+default[:boundary][:api][:hostname] = "api.boundary.com"
+default[:boundary][:api][:org_id] = "org_id"
+default[:boundary][:api][:key] = "api_key"
+default[:boundary][:hostname] = node[:fqdn]
---
+++ attributes/meter.rb
@@ -0,0 +1,8 @@
+default[:boundary][:meter][:bin_path] = "/usr/bin"
+default[:boundary][:meter][:etc_path] = "/etc/boundary"
+default[:boundary][:meter][:collector][:hostname] = "collector.boundary.com"
+default[:boundary][:meter][:collector][:port] = "4740"
+
+default[:boundary][:meter][:tags] = []
+
+default[:boundary][:meter][:interfaces] = nil
